5511a8e6b2da8cd82dea97779df8edaf4a4cea31
marcogarcia96 <marcogarcia831@gmail.com>
2021-04-23 10:43:19 -0700

    added server connection and data base connection

---
+++ server/server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.get('/',(req,res)=>{
+  res.send('welcome to Server Side Set up')
+})
+
+app.get('/products', Controller.Products)
+
+app.get(`/products/:product_id`,Controller.Product)
+
+app.get(`/products/:product_id/related`, Controller.Related)
+
+app.get(`/products/:product_id/styles`, Controller.Styles)
+
+app.listen(port, () => {
+  console.log(`listening at http://localhost:${port}`)
+})
