d37adb878cca6705f6059fcf06b67e03d2c86984
marcogarcia96 <marcogarcia831@gmail.com>
2021-04-25 14:42:19 -0700

    completed the server and querries in controller

--- server/Controller.js
+++ server/Controller.js
@@ -60,8 +60,71 @@ module.exports = {
     }
   })} ,
   Styles:(req,res)=>{
+    const styles = new Promise((resolve, reject) => {
+      DB.connection.query(`select * from Styles where product_id = ${req.params.product_id}`,(err,data)=>{
+        if(err){
+          reject(err)
+        }else{
+          resolve(data)
+        }
+      })
+    })
+    const photo = new Promise((resolve, reject) => {
+      DB.connection.query(`select * from Photo where style_id in (select style_id from Styles where product_id = ${req.params.product_id});`,(err,data)=>{
+        if(err){
+          reject(err)
+        }else{
+          resolve(data)
+        }
+      })
+    })
+    const skus = new Promise((resolve, reject) => {
+      DB.connection.query(`select * from Skus where style_id in (select style_id from Styles where product_id = ${req.params.product_id});`,(err,data)=>{
+        if(err){
+          reject(err)
+        }else{
+          resolve(data)
+        }
+      })
+    })
 
 
+
+    styles.then((styl)=>{
+      photo.then((pho)=>{
+        skus.then((sk)=>{
+          var Allskus = Object.values(JSON.parse(JSON.stringify(sk)))
+          var Allphotos = Object.values(JSON.parse(JSON.stringify(pho)))
+          var Allstyles = Object.values(JSON.parse(JSON.stringify(styl)))
+
+
+          Allstyles.forEach((obj,i)=>{
+            if(obj.default_style.data[0] === 48){
+              obj.default_style = false;
+            } else {
+              obj.default_style = true;
+            }
+            var arrayofphotos = [];
+            var arrayofskus = [];
+            for(var i = 0; i < Allphotos.length; i++ ){
+              if(Allphotos[i].style_id === obj.style_id){
+                var hold = Allphotos.splice(i,1)
+                arrayofphotos.push(hold[0])
+              }
+            }
+
+            for(var i = 0; i < Allskus.length; i++ ){
+              var hold = Allskus.splice(i,1)
+              arrayofskus.push(hold[0])
+            }
+            obj.photos = arrayofphotos
+            obj.skus = arrayofskus
+
+          })
+          res.send(Allstyles)
+        })
+      })
+    })
 
 
   }
