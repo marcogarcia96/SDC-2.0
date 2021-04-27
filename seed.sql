e69991f8eb0a0c80bf11744f8ff3f95e8d1fdd6a
marcogarcia96 <marcogarcia831@gmail.com>
2021-04-27 16:56:59 -0700

    put instance in docker

--- seed.sql
+++ seed.sql
@@ -1,38 +1,38 @@
-LOAD DATA LOCAL INFILE '/database/cleandata/cleanproduct.csv'
+LOAD DATA LOCAL INFILE '/data/cleanproduct.csv'
 INTO TABLE Product
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/home/marco_ga/SDC-2.0/database/cleandata/cleanrelated.csv'
+LOAD DATA LOCAL INFILE '/data/cleanrelated.csv'
 INTO TABLE related_product
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/home/marco_ga/SDC-2.0/database/cleandata/cleanfeatures.csv'
+LOAD DATA LOCAL INFILE '/data/cleanfeatures.csv'
 INTO TABLE Features
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n';
 
-LOAD DATA LOCAL INFILE '/home/marco_ga/SDC-2.0/database/cleandata/cleanstyles.csv'
+LOAD DATA LOCAL INFILE '/data/cleanstyles.csv'
 INTO TABLE Styles
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/home/marco_ga/SDC-2.0/database/cleandata/cleanskus.csv'
+LOAD DATA LOCAL INFILE '/data/cleanskus.csv'
 INTO TABLE Skus
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
 LINES TERMINATED BY '\n'
 IGNORE 1 ROWS;
 
-LOAD DATA LOCAL INFILE '/home/marco_ga/SDC-2.0/database/cleandata/cleanphotos.csv'
+LOAD DATA LOCAL INFILE '/data/cleanphotos.csv'
 INTO TABLE Photo
 FIELDS TERMINATED BY ','
 ENCLOSED BY '"'
--- server/connectiondb.js
+++ server/connectiondb.js
@@ -1,11 +1,19 @@
-const mysql = require('mysql')
+const mysql = require('mysql2')
 
-var connection = mysql.createConnection({
-  host:'localhost',
+var connection = mysql.createPool({
+  host:'database',
   user: 'root',
-  password: '',
   database: 'Product_DB',
   multipleStatements: true
 })
+
+// connection.connect((err)=>{
+// if(err){
+//   console.log(err)
+// }else {
+//   console.log('Connected to the MySQL server.')
+// }
+
+// });
 
 module.exports.connection = connection;
